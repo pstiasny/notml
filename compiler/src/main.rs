46663d5217822931d3169dc0d501c5040f7ff885
Paweł Stiasny <pawelstiasny@gmail.com>
2020-05-06 21:19:22 +0200

    improve CLI usability

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,19 +1,22 @@
 #![deny(warnings)]
 
-extern crate compiler;
+extern crate notmlc;
 extern crate toml;
+extern crate serde;
+extern crate clap;
 
-use std::io::{self, Read};
+use std::io::{Read};
 use std::fs::File;
 use std::process::{Command, Stdio, exit};
 
+use clap::{Arg, App};
 use serde::{Deserialize};
 
-use compiler::lexer::{lex, trim_ws};
-use compiler::parser::parse;
-use compiler::sem::annotate;
-use compiler::codegen::write_amd64;
-use compiler::interpreter::eval;
+use notmlc::lexer::{lex, trim_ws};
+use notmlc::parser::parse;
+use notmlc::sem::annotate;
+use notmlc::codegen::write_amd64;
+use notmlc::interpreter::eval;
 
 
 #[derive(Deserialize)]
@@ -80,13 +83,49 @@ fn linker_args<'a>(
 
 
 fn main() -> std::io::Result<()> {
+    let arg_matches = App::new("NotML compiler")
+        .author("Pawel Stiasny <pawelstiasny@gmail.com>")
+        .about("Toy compiler for the toy NotML language")
+        .arg(Arg::with_name("config")
+             .short("-c")
+             .value_name("FILE")
+             .help("Compiler config TOML file")
+             .takes_value(true))
+        .arg(Arg::with_name("INPUT")
+            .help("Source file")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("output")
+             .short("-o")
+             .value_name("FILE")
+             .help("Binary file path"))
+        .get_matches();
+
     let mut config_str = String::new();
-    let mut config_file = File::open("config.toml")?;
+    let config_path = arg_matches.value_of("config").unwrap_or("config.toml");
+    let mut config_file = File::open(config_path)?;
     config_file.read_to_string(&mut config_str)?;
     let config: Config = toml::from_str(&config_str)?;
 
+    let input_path: String = arg_matches.value_of("INPUT").unwrap().to_string();
+    let mut input_file = File::open(&input_path)?;
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
+    input_file.read_to_string(&mut input)?;
+
+    let base_path = if let Some(s) = arg_matches.value_of("output") {
+        s.to_string()
+    } else if input_path.ends_with(".notml") {
+        input_path[0..input_path.len() - 6].to_string()
+    } else {
+        input_path.clone()
+    };
+    let asm_path = format!("{}.asm", base_path);
+    let object_path = format!("{}.o", base_path);
+    let output_path = if base_path == input_path {
+        format!("{}.out", base_path)
+    } else {
+        base_path.clone()
+    };
 
     let lex_result = lex(&input);
     println!("Tokens: {:?}", lex_result);
@@ -102,13 +141,13 @@ fn main() -> std::io::Result<()> {
             if let Ok(at) = ares {
                 println!("Eval: {:?}", eval(&pt));
 
-                let mut outfile = File::create("out.asm")?;
+                let mut outfile = File::create(&asm_path)?;
                 write_amd64(&at, &mut outfile)?;
 
-                run_cmd("nasm", &nasm_args(&config, "out.asm", "out.o"))?;
-                run_cmd("ld", &linker_args(&config, "out.o", "out"))?;
+                run_cmd("nasm", &nasm_args(&config, &asm_path, &object_path))?;
+                run_cmd("ld", &linker_args(&config, &object_path, &output_path))?;
 
-                println!("done");
+                println!("Done 💖");
                 return Ok(());
             }
         }
